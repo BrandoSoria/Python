4809049e0eaceb3719b1de12eaed6ca9ccae061d
brandon <webarebrandy@gmail.com>
2024-06-10 23:49:19 -0600

    variables

---
+++ format.py
@@ -0,0 +1,2 @@
+"""Module providing a function printing python version."""
+Chanchito = "chanchito feliz"
---
+++ variables.py
@@ -0,0 +1,10 @@
+"""Variables"""
+nombre_curso = "Curso de Python" 
+print(nombre_curso)
+
+Numero1 = 10
+Numero2 = 20
+Numero3 = 30.7
+publicado = True
+publicado = False
+print(Numero1 + Numero2)
