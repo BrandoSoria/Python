b49312995a0a6924e2b06c27e33c406aee71273f
brandon <webarebrandy@gmail.com>
2024-06-11 00:06:29 -0600

    formatear strings de una mejor forma

---
+++ tipos/format-strings.py
@@ -0,0 +1,9 @@
+"""Format Strings."""
+
+nombre = "nicolas"
+apellido = "hernandez"
+#mejor forma de concatenar strings
+nombre_completo = f"{nombre[0]} {2+5}"
+# acceso a la informacion y poner lo que quieras
+nombre_completo = f"{nombre[0]} {2+5}"
+print(nombre_completo)
