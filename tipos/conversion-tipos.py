36703baf74c122f05b4b924bb61b574d62ec5b36
brandon <webarebrandy@gmail.com>
2024-06-11 23:37:23 -0600

    como convertir tipos de datos

---
+++ tipos/conversion-tipos.py
@@ -0,0 +1,18 @@
+"""conversion-tipos"""
+
+# int()
+
+# float() decimal
+
+# str() string
+
+# bool() boolean true or false
+
+print(bool (""))
+print(bool ("1"))
+print(bool (1))
+print(bool ("0"))
+print(bool (0))
+print(bool (0.0))
+print(bool (None))
+print(bool ("False"))
