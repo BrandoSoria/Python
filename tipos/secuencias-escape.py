381a6ea920774b2efb50a044cf746ad5c7a4e970
brandon <webarebrandy@gmail.com>
2024-06-11 23:02:49 -0600

    secuencias de escape

---
+++ tipos/secuencias-escape.py
@@ -0,0 +1,11 @@
+
+#secuencias de escape
+# \n nueva linea
+# \t tabulador
+# \\ barra invertida
+# \' comilla simple
+# \" comilla doble
+# \a sonido
+
+curso = "Ultimate \n Python\""
+print(curso)
