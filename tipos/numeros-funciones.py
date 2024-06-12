ece15da5280f605202ee492d7f832cf060cef382
brandon <webarebrandy@gmail.com>
2024-06-11 23:21:22 -0600

    funciones matematicas

---
+++ tipos/numeros-funciones.py
@@ -0,0 +1,19 @@
+import math
+
+#muestra el numero que se encuentra mas cerca
+print(round(1.3))
+print(round(1.7))
+# valor absoluto siempre es positivo
+print(abs(-1.3))
+print(abs(1.3))
+
+#redondea hacia arriba
+print(math.ceil(1.3))
+#redondea hacia abajo
+print(math.floor(1.3))
+
+# print(math.isnan(13))
+# raiz cuadrada
+print(math.sqrt(9))
+# potenciar numeros
+print(math.pow(2, 3))
