259593aecd202f8cc36d91f2bcba6c903d1ef8e7
brandon <webarebrandy@gmail.com>
2024-06-11 23:12:46 -0600

    numeros

---
+++ tipos/numeros.py
@@ -0,0 +1,32 @@
+numero = 2 # int
+numero2 = 2.0 # float
+numero3 = 2.1e10 # float
+numero4 = 2.1e-10 # float
+# tipo de dato de la variale
+print(type(numero))
+
+print("el numero es: ", numero)
+print("el numero es: ", numero2)
+print("el numero es: ", numero3)
+print("el numero es: ", numero4)
+
+numeroa = numero2 +2
+# print("el numero es: ", numeroa)
+# incremento de una de lo de la izquierda
+numeroa += 2
+print("el valor es es: ", numeroa)
+# imprimir el valor de dos variables en una solo print
+print(f"{numero = } {numero3 = }")
+
+
+print(1 + 2)
+print(1 - 2)
+print(1 * 2)
+# division
+print(1 / 2)
+# cociente
+print(1 // 2)
+# residuo
+print(1 % 2)
+# exponente
+print(2 ** 3)
