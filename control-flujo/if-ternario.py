9586c5e56d85de0f95a515f4323268c848c550e5
brandon <webarebrandy@gmail.com>
2024-06-12 00:59:15 -0600

    condicion if ternario

---
+++ control-flujo/if-ternario.py
@@ -0,0 +1,10 @@
+edad = 18
+# if ternario
+mensaje = "es mayor de edad" if edad > 17 else "es menor de edad"
+
+# # if normal
+# if edad > 17:
+#     mensaje = "es mayor de edad"
+# else:
+#     mensaje = "es menor de edad"
+print(mensaje)
