5cfe1e5401857940427d9d02a29b114bd3802cd9
brandon <webarebrandy@gmail.com>
2024-06-12 23:18:39 -0600

    ciclo while

---
+++ control-flujo/while.py
@@ -0,0 +1,12 @@
+# # si se cumple que al multiplicar un numero por dos es menor a 100 imprime ese numero
+# numero = 1
+# while numero < 100:
+#     print(numero)
+#     numero *=2
+
+#muestra los que teclea el usuario
+comando = ""
+while comando.lower() != "salir":
+    comando = input("$ ")
+    print(comando)
+    
