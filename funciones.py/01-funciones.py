4afb3f01fdab380c1eb51b7cd6c2d186f2175d0a
brandon <webarebrandy@gmail.com>
2024-06-13 23:45:52 -0600

    uso de parametros y argumentos en una funcion

--- funciones.py/01-funciones.py
+++ funciones.py/01-funciones.py
@@ -1,4 +1,7 @@
-def hola():
+#definicion de la funcion y su parametro y ahora otro parameyro
+def hola(nombre, apellido):
     print("hola mundo")
-    print("ultima linea")
-hola()
+    print(f"bienvenido a la clase {nombre} {apellido}") #uso del parametro
+#ejecucion de la funcion y su argumento
+hola("brandon", "gonzalez")
+hola("nicolas", "hernandez")
