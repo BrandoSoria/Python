e629dcb88b4978cb707c5d0888729b9c12cc506b
brandon <webarebrandy@gmail.com>
2024-06-13 23:48:49 -0600

    argumentos opcionales

--- funciones.py/01-funciones.py
+++ funciones.py/01-funciones.py
@@ -1,7 +1,10 @@
-#definicion de la funcion y su parametro y ahora otro parameyro
-def hola(nombre, apellido):
+#definicion de la funcion y su parametro y valor por defecto
+def hola(nombre, apellido= "soria"):
     print("hola mundo")
     print(f"bienvenido a la clase {nombre} {apellido}") #uso del parametro
 #ejecucion de la funcion y su argumento
 hola("brandon", "gonzalez")
-hola("nicolas", "hernandez")
+hola("nicolas" )
+
+# el parametro apellido que tiene el valor por defecto es opcional
+# y si no se le pasa un valor para el argumento se toma el valor por defecto del parametro
