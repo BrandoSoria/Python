f04ad7feb34e8d75224ac1c299247ff820dbfc91
brandon <webarebrandy@gmail.com>
2024-06-15 22:37:09 -0600

    alcance

---
+++ funciones.py/05-alcance.py
@@ -0,0 +1,17 @@
+saludo="hola global"
+
+def saludar():
+    saludo = "hola"
+    print(saludo)
+
+
+def saludaChanchito():
+    saludo = "hola chanchito feliz"
+    print(saludo)
+    
+
+saludar()
+print(saludo)
+saludar()
+print(saludo)
+
