2739bc92ba488fbc05f6eedbb4998d6d33dd6cd1
brandon <webarebrandy@gmail.com>
2024-06-18 19:40:17 -0600

    iterando listas, usando indices

---
+++ listas/iterando-listas.py
@@ -0,0 +1,6 @@
+mascotas=["gato","perro","pajaro,","loro","caballo","vaca"]
+
+#mostrando la lista de los elementos ademas los enumeramos usando un indice
+for indice, mascota in enumerate (mascotas):
+    print(indice ,mascota)
+    
