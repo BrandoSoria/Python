9dd4b316d1589dc15084b5e665244f7a16af2db3
brandon <webarebrandy@gmail.com>
2024-06-18 18:45:03 -0600

    manipulando listas

---
+++ listas/manipulando-listas.py
@@ -0,0 +1,17 @@
+mascotas=["gato","perro","pajaro,","loro","caballo","vaca"]
+#accede al elemento que se encuentra en la posicion 1
+print(mascotas[1])
+#modifica el elemento que se encuentra en la posicion 1
+mascotas[1]="loro"
+print(mascotas)
+# #mostrar la lista desde la posicion 0 hasta la 3
+# print(mascotas [0:3])
+# #mostrar la lista desde la posicion 0 hasta la 2
+# print(mascotas [:-2])
+# #mostrar la lista desde la posicion 0 hasta la 3 saltando de 2 en 2 por los doble ::
+# print(mascotas [1::2])
+
+
+numeros=list(range(21))
+#mostrar numeros par
+print(numeros[::2])
