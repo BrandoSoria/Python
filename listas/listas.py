d9366f88bdafec608282a53de64ec7aa7e4839ed
brandon <webarebrandy@gmail.com>
2024-06-18 18:35:58 -0600

    aprendiendo a usar listas

---
+++ listas/listas.py
@@ -0,0 +1,34 @@
+numeros = [1,2,3]
+letras = ["a","b","c"]
+palabras = ["hola","chanchito","feliz"]
+palabrasfeliz = ["feliz","contento","alegre"]
+print(palabrasfeliz)
+
+booleans = [True, False, True]
+print(booleans)
+
+matriz=[[0,1],[2,3]]
+print(matriz)
+
+#listas dentro de listas
+matriz2 = [[palabrasfeliz],[booleans]]
+print(matriz2)
+
+cerosUnos=[0,1]*10
+print(cerosUnos)
+
+ceros=[0,0]*10
+print(ceros)
+
+# junta de dos listas en una
+alfanumericos= letras+numeros
+print(alfanumericos)
+
+#rango de numeros desde 1 a 10
+rango=list(range(1,11))
+print(rango)
+
+#mostrar cada letra de un string en una lista
+chars= list("chanchito")
+print(chars)
+
