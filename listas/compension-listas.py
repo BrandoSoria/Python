1b83474a99b540aca6b017609ca30d9a29009521
brandon <webarebrandy@gmail.com>
2024-06-19 20:30:16 -0600

    filtrar y transformar la lista e el mismo archivo

--- listas/compension-listas.py
+++ listas/compension-listas.py
@@ -13,6 +13,21 @@ usuarios= [
 
 # print(nombres)
 
-#otra forma de mostrar solo el nombre de los usuarios
-nombres = [usuario[0] for usuario in usuarios]
+#otra forma de mostrar solo el nombre de los usuarios, es decir lo transformo en una lista
+# se transforma la lista para mostrar solo el nombre de usuarios y no el id de usuario
+# se usa el parametro con usuario[0]
+
+# nombres = [usuario[0] for usuario in usuarios]
+
+
+#filtrar de manera que solo me muestre los usuarios que tengan un id mayor a 2
+
+# nombres=[usuario for usuario in usuarios if usuario[1] > 2]
+
+
+#filtrar y transformar la lista
+# se transforma la lista para mostrar solo el nombre de usuarios y no el id de usuario
+# se usa el parametro con usuario[0]
+
+nombres=[usuario[0] for usuario in usuarios if usuario[1] > 2]
 print(nombres)
