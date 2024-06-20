a2fc1b1a2ef971ecf1e4676f39504b4ee86a1983
brandon <webarebrandy@gmail.com>
2024-06-19 19:49:23 -0600

    agregar y eliminar elementos

---
+++ listas/agregar-eliminando.py
@@ -0,0 +1,31 @@
+#agregar y eliminar elementos de una lista
+
+mascotas=["gato",
+          "perro",
+          "pajaro",
+          "loro",
+          "caballo",
+          "vaca"
+          
+          ]
+#agregando un elemento en el indice 1
+# mascotas.insert(1,"conejo")
+#agregando un elemento al final de la lista
+mascotas.append("oso")
+print(mascotas)
+
+#eliminando un elemento especifico de la lista
+# mascotas.remove("pajaro")
+
+#eliminando el ultimo elemento de la lista
+# mascotas.pop()
+
+#eliminando un elemento en el indice 1
+# mascotas.pop(1)
+
+#eliminando un elemento en el indice 1 otra opcion
+del mascotas[1]
+
+#eliminando todos los elementos de la lista
+mascotas.clear()
+print(mascotas)
